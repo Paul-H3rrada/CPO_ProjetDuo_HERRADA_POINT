edef376531c53f927b5a575cce5972c96be11f6a
thomaspoint <thomaspoint@localhost>
2024-11-20 12:36:51 +0100

    classe GrilleDeJeu avancement

--- Demineur_HERRADA_POINT/src/demineur_herrada_point/GrilleDeJeu.java
+++ Demineur_HERRADA_POINT/src/demineur_herrada_point/GrilleDeJeu.java
@@ -1,13 +1,50 @@
+package demineur_herrada_point;
+
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package demineur_herrada_point;
+
 
 /**
  *
  * @author thomaspoint
  */
 public class GrilleDeJeu {
+    private Cellule[][] grille;
+    private int nbLignes;
+    private int nbColonnes;
+    private int nbBombes;
+    
+    public GrilleDeJeu(int nbLignes, int nbColonnes, int nbBombes){
+        this.nbLignes = nbLignes;
+        this.nbColonnes = nbColonnes;
+        this.nbBombes = nbBombes;
+        this.grille = new Cellule[nbLignes][nbColonnes];
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                grille[i][j] = new Cellule();
+            }
+        }
+    }
+    public int getNbLignes() {
+        return nbLignes;
+    }
+
+    public int getNbColonnes() {
+        return nbColonnes;
+    }
+
+    public int getNbBombes() {
+        return nbBombes;
+    }
+    
+    public void placerBombesAleatoirement() {
+        Random rand = new Random();
+        int bombesPlacees = 0;
+    }
     
 }
+
