fcc74db2c9a06ccb10defa616eb690a8540c9485
Paul Herrada <paul.herrada@epfedu.fr>
2024-11-27 10:11:55 +0100

    premier test du main(), la grille s'affiche mais rien ne se passe (les cases ne se révèlent pas et le jeu crash lorsque nous découvrons une bombe)

--- Demineur_HERRADA_POINT/src/demineur_herrada_point/Demineur_HERRADA_POINT.java
+++ Demineur_HERRADA_POINT/src/demineur_herrada_point/Demineur_HERRADA_POINT.java
@@ -9,6 +9,9 @@ package demineur_herrada_point;
  * @author herra
  */
 public class Demineur_HERRADA_POINT {
-
+  public static void main(String[] args) {
+        Partie partie = new Partie(7, 7, 25); // Instanciation de la partie
+        partie.demarrerPartie();             // Démarrage de la partie
+    }
     
 }
